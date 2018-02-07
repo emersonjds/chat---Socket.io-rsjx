f3e179bc1ecdaeacc66cc9f1be02203c8b15ba12
Emerson Silva <emerson_jdss@hotmail.com>
2018-02-07 21:24:46 -0200

    finish server.js

--- server.js
+++ server.js
@@ -4,4 +4,16 @@ let io = require('socket.io')(http);
 
 io.on('connect', (socket) => {
     console.log('user connected');
-})
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('on-message', (message) => {
+        io.emit('message', {type: 'new-message', text: 'message'})
+    });
+});
+
+http.listen(5000, () => {
+    console.log('start server on port 5000');
+});
